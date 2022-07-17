9ac6f0cf79a1127885f80b50c307b2219e3120b3
md-alif10000 <md.alif10000@gmail.com>
2022-07-17 20:39:38 +0600

    added dynamic url for blog

--- src/pages/Blog/BlogCard.jsx
+++ src/pages/Blog/BlogCard.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import slugify from "slugify";
 
 const BlogCard = ({ item }) => {
   return (
@@ -17,7 +18,7 @@ const BlogCard = ({ item }) => {
         </div>
         <div className="blog-content px-6">
           <h3 className="font-display text-xl text-blueGray-900 font-bold transition hover:text-indigo-500">
-            <Link to="/single">{item.title}</Link>
+            <Link to={`/${slugify(item.title)}`}>{item.title}</Link>
           </h3>
           <div className="flex flex-wrap font-body text-sm text-blueGray-600 mt-3 mb-5">
             <p className="mr-4">
@@ -45,7 +46,7 @@ const BlogCard = ({ item }) => {
       <div className="content-bottom px-6 pb-6">
         <Link
           className="readmore-btn font-body font-normal text-indigo-500 transition duration-500 hover:text-indigo-500 underline-hover"
-          to="/single"
+          to={`/${slugify(item.title)}`}
         >
           Read More
           <img
